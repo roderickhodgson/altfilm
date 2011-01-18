bfa62f89915cdc97a9178a24f70270fdf716d6d7
Roderick Hodgson <roderick@li196-234.members.linode.com>
2011-01-18 21:43:29 +0000

    Fixes to work better with Geolocate

--- refresh_db.py
+++ refresh_db.py
@@ -6,25 +6,26 @@ f = open(ukfc_data)
 def populate():
     hundrediter = 0
     for l in f:
-        name = address = ""
-        r = csv.reader(l, delimiter=',', quotechar='"')
-        it = 0
-        for c in r:
-            if it==8:
-                name = c[0]
-            if it==14:
-                address += c[0]
-            if it==18:
-                address += ", "+ c[0]
-            if it==20:
-                address += ", "+ c[0]
-            it += 1
+        if hundrediter > 897000:
+		name = address = ""
+		r = csv.reader(l, delimiter=',', quotechar='"')
+		it = 0
+		for c in r:
+		    if it==8:
+			name = c[0]
+		    if it==14:
+			address += c[0]
+		    if it==18:
+			address += ", "+ c[0]
+		    if it==20:
+			address += ", "+ c[0]
+		    it += 1
 
-        hundrediter += 1
-        if hundrediter % 100 == 0:
-            print hundrediter 
-        
-        venue, p= Venue.objects.get_or_create(name=name, address=address)
-        venue.save()
+		
+		venue, p= Venue.objects.get_or_create(name=name, address=address)
+		venue.save()
 
+	hundrediter += 1
+	if hundrediter % 100 == 0:
+	    print hundrediter 
 
